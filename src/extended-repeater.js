ad9c1d199dc6aa6ec70355b1c15acdcf8aa6ca64
samael-god <nikita.95_@mail.ru>
2023-04-17 19:18:41 +0300

    feat: extended-repeater solves

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,49 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
+ *
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
 function repeater(str, options) {
-  let result = '';
+    let result = '';
 
-  if(options.addition){
-    if(options.additionRepeatTimes > 1){
-      for(let i = 1; i <= options.additionRepeatTimes; i++){
-        options.additionSeparator ? str += options.addition + options.additionSeparator : str += options.addition + '|';
+    if (options.addition || typeof options.addition === "boolean" || typeof options.addition === "object") {
+        if (options.additionRepeatTimes > 1) {
+            for (let i = 1; i <= options.additionRepeatTimes; i++) {
+                (options.additionSeparator) ? (i !== options.additionRepeatTimes) ? (str += options.addition + options.additionSeparator) : str += options.addition : (i !== options.additionRepeatTimes) ? str += options.addition + '|' : str += options.addition;
+            }
+        } else {
+            str += options.addition;
+        }
+    }
+    console.log(str)
+    result += str;
+    if (options.repeatTimes) {
+        for (let i = 1; i < options.repeatTimes; i++) {
+            if (options.separator) {
+                result += options.separator + str;
+            } else {
+                result += '+' + str;
+            }
+        }
+    }
 
-      }
-    } else{
-      str += options.addition;
-    }
-  }
-  result += str;
-if(options.repeatTimes){
-  console.debug(result, str)
-  for (let i = 1; i < options.repeatTimes; i++){
-    if (options.separator){
-      result += options.separator + str;
-    } else{
-      result += '+' + str;
-    }
-  }
-}
-console.log(result)
-return  result
+    return result
 }
 
 module.exports = {
 
-  repeater
+    repeater
 };
